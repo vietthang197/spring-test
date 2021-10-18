5629615c0ba640f5f328c44b76f026082acaf6d9
acer <thanglv@neo.vn>
2021-10-18 15:08:03 +0700

    update

--- src/main/java/com/thanglv/springtest/socket/handler/PrintHandler.java
+++ src/main/java/com/thanglv/springtest/socket/handler/PrintHandler.java
@@ -24,7 +24,6 @@ public class PrintHandler extends Thread {
         } catch (IOException e) {
             return;
         }
-        String line = null;
         while (isRun && client.isConnected()) {
             try {
                 int nRead;
