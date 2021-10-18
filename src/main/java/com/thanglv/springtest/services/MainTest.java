4d081c7a079dbdf2e35e0a11bcdbd69c0b22e6c2
thanglv <levietthang1997@gmail.com>
2021-10-18 13:24:17 +0700

    update code

--- src/main/java/com/thanglv/springtest/services/MainTest.java
+++ src/main/java/com/thanglv/springtest/services/MainTest.java
@@ -7,7 +7,7 @@ import java.io.FileNotFoundException;
 
 public class MainTest {
     public static void main(String[] args) throws PrinterException, FileNotFoundException {
-        PrintService sp46Printer = PrintProvider.getPrinterByName("Microsoft Print to PDF");
+        PrintService sp46Printer = PrintProvider.getPrinterByName("SP46");
         PrinterJob printerJob = PrinterJob.getPrinterJob();
         printerJob.setPrintService(sp46Printer);
         Book book = new Book();
@@ -15,9 +15,10 @@ public class MainTest {
 
         // set paper a6
         Paper a6Paper = new Paper();
-        double paperWidth = 4.1;
-        double paperHeight = 5.8;
-        a6Paper.setSize( paperWidth * 72.0, paperHeight * 72.0 );
+        double paperWidth = 4.1; // size a6 inches
+        double paperHeight = 5.8; // size a6 inches
+        a6Paper.setSize(paperWidth * 72.0, paperHeight * 72.0);
+        a6Paper.setImageableArea(0, 0, paperWidth * 72.0, paperHeight * 72.0);
         //
         pageFormat.setPaper(a6Paper);
         book.append(new ImagePrinter(new FileInputStream("D:/test.png")), pageFormat);
